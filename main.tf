288cb693c06a24b7257b6d2ba7659fc9830481ae
parth sharma <parthsharma1011@gmail.com>
2025-09-29 23:09:38 +0200

    Add Terraform infrastructure configuration

---
+++ main.tf
@@ -0,0 +1,49 @@
+# VPC and networking
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Security group
+resource "aws_security_group" "web" {
+  name_prefix = "web-"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance
+resource "aws_instance" "web" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web.id]
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              EOF
+              
+  tags = {
+    Name = "web-server"
+  }
+}
